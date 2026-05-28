bd9a4de01d0097c8434f1fbeb2d8a7c963cd5fba
francescogent-beep <francesco_gent@yahoo.it>
2026-05-28 15:34:06 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   images: {
     formats: ['image/webp', 'image/avif'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 }
 export default nextConfig
